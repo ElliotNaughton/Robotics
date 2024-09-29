4492bd969d53fb2de54d1dd791e4e224720ec5ee
ElliotNaughton <naugheaa@uni.edu>
2024-09-28 23:37:58 -0500

    Moved debounce timer to ultraSonic.objectDetected()

--- RoboticsTeamESP32/src/main.cpp
+++ RoboticsTeamESP32/src/main.cpp
@@ -10,16 +10,13 @@ motorControl robotMove;
 ultraSonic sensor;
 
 int ps3StickDeadzone = 4; //what number to start tracking value changes for analog stick reading
+int plowLiftPin = 20; //pin for relay to actuators to lift plow
+bool plowState = false; //tracks state of plow, either up (true) or down (false)
 
 bool objectRumble = true; //set to false after rumble when object is detected and true when there is no object, ensures controller does not rumble the entire time an object is detected
-bool objectDebounce = false;
 
-void ps3ReadPwmWrite(void * parameter); //task to read from ps3 controller and write to motor pwm
+void ps3ReadEsp32Execute(void * parameter); //task to read from ps3 controller and write to motor pwm
 void continuosDetection(void * parameter); //task to read and pulse ultrasonic
-
-unsigned long globalCurrentMillis; //current milliseconds variable for main program
-unsigned long objectDetectPrevMillis; //previous milliseconds for object detect debounce timer
-long objectDetectDebounceInterval = 1000; //millisecond interval for object detect debounce timer
 
 void setup() {
 
@@ -46,6 +43,7 @@ void setup() {
 
   sensor.normalDuration = 0; //TBD normal duration of ultrasonic pulse at floor distance
   sensor.sensorTrigDelay = 50; //trigger delay in milliseconds, research shows will dissapate in 50 ms
+  sensor.sensorDebounce = 1000; //debounce timer in ms for objectDetection in plow
 
   //trig pin setup
   sensor.trigPin1 = 16;
@@ -73,11 +71,12 @@ void setup() {
 	
   Serial.begin(115200); //starting serial communication for debugging at 115200 baud
   Ps3.begin("2c:81:58:49:e4:01"); //beginning PS3 console emulation at mac adress specified
+  pinMode(plowLiftPin ,OUTPUT); //plow lift relay pin set up
 
 /////////////////////////////////////////////////////////////////// free RTOS task creation //////////////////////////////////////////////
   
   xTaskCreate(//task to read from ps3 controller and write to motor pwm
-      ps3ReadPwmWrite,    // Function that should be called
+      ps3ReadEsp32Execute,    // Function that should be called
       "PS3 Read PWM Write",  // Name of the task (for debugging)
       10000,            // Stack size (bytes)
       NULL,            // Parameter to pass
@@ -105,7 +104,7 @@ void loop()
 //empty loop will not run do to use of freeRTOS tasks
 }
 
-void ps3ReadPwmWrite(void * parameter){//task to read from ps3 controller and write to motor pwm
+void ps3ReadEsp32Execute(void * parameter){//task to read from ps3 controller and write to motor pwm
   for(;;){//start infinite loop freeRTOS can break out of loop for other tasks
 
   if (Ps3.isConnected()){ //test for connection print to serial line
@@ -115,7 +114,7 @@ void ps3ReadPwmWrite(void * parameter){//task to read from ps3 controller and wr
   //Horizontal Movement of the Left Stick
   if(Ps3.event.analog_changed.stick.lx){
 
-      if(Ps3.data.analog.stick.lx > ps3StickDeadzone){robotMove.rightShift(Ps3.data.analog.stick.lx);} //stick value greater than deadzon
+      if(Ps3.data.analog.stick.lx > ps3StickDeadzone){robotMove.rightShift(Ps3.data.analog.stick.lx);}
 
       if(Ps3.data.analog.stick.lx < -ps3StickDeadzone){robotMove.leftShift(Ps3.data.analog.stick.lx);}
   }
@@ -136,26 +135,27 @@ void ps3ReadPwmWrite(void * parameter){//task to read from ps3 controller and wr
     if(Ps3.data.analog.stick.rx < -ps3StickDeadzone){robotMove.CCW(Ps3.data.analog.stick.rx);} 
   }  
 
-    //Vertical movement of the Right Stick
-    if(Ps3.event.analog_changed.stick.ry){
+  //Vertical movement of the Right Stick
+  if(Ps3.event.analog_changed.stick.ry){
 
 
-  }  
+    }  
   }
+
+  //right bumper
+  if(Ps3.event.button_down.r1){ 
+    if(plowState) {digitalWrite(plowLiftPin,LOW); plowState = false;}//toggle plow up and down
+    else {digitalWrite(plowLiftPin,HIGH); plowState = true;}
+
+
+}
 }
 
 void continuosDetection(void * parameter){//task to read and pulse ultrasonic
   for(;;){ //start infinite loop freeRTOS can break out of loop for other tasks
     sensor.currentMillis = millis();
-    globalCurrentMillis = millis();
-
+ 
     if(sensor.objectDetected()){
-      objectDebounce = true;
-      objectDetectPrevMillis = millis();
-    }
-    else if (globalCurrentMillis - objectDetectDebounceInterval > objectDetectPrevMillis){objectDebounce = false;}
-
-    if(objectDebounce){
       Ps3.setPlayer(4);
 
       if (objectRumble){Ps3.setRumble(50,1);}
--- RoboticsTeamESP32/src/ultraSonic.cpp
+++ RoboticsTeamESP32/src/ultraSonic.cpp
@@ -43,22 +43,29 @@ void ultraSonic::runAtSetup(){
 
 bool ultraSonic::objectDetected(){
 
-    if (currentMillis - sensorTrigDelay > prevMillis){ //if it has been delay time since last run, run again
+    if (currentMillis - sensorTrigDelay > prevMillisPulse){ //if it has been delay time since last run, run again
 
-        //can only pulse one sensor every delay to allow for correct measurement, if there is anything detected return true
-             if(sensorIndex == 1) {return singleSensorDetect(trigPin1, echoPin1) ? true : false;}
-        else if(sensorIndex == 2) {return singleSensorDetect(trigPin2, echoPin2) ? true : false;}
-        else if(sensorIndex == 3) {return singleSensorDetect(trigPin3, echoPin3) ? true : false;}
-        else if(sensorIndex == 4) {return singleSensorDetect(trigPin4, echoPin4) ? true : false;}
-        else if(sensorIndex == 5) {return singleSensorDetect(trigPin5, echoPin5) ? true : false;}
-        else if(sensorIndex == 6) {return singleSensorDetect(trigPin6, echoPin6) ? true : false;}
-        else if(sensorIndex == 7) {return singleSensorDetect(trigPin7, echoPin7) ? true : false;}
-        else if(sensorIndex == 8) {return singleSensorDetect(trigPin8, echoPin8) ? true : false;}
+        //can only pulse one sensor every delay to allow for correct measurement, if there is anything detected objectInPlow = true
+             if(sensorIndex == 1) {objectInPlow = singleSensorDetect(trigPin1, echoPin1);}
+        else if(sensorIndex == 2) {objectInPlow = singleSensorDetect(trigPin2, echoPin2);}
+        else if(sensorIndex == 3) {objectInPlow = singleSensorDetect(trigPin3, echoPin3);}
+        else if(sensorIndex == 4) {objectInPlow = singleSensorDetect(trigPin4, echoPin4);}
+        else if(sensorIndex == 5) {objectInPlow = singleSensorDetect(trigPin5, echoPin5);}
+        else if(sensorIndex == 6) {objectInPlow = singleSensorDetect(trigPin6, echoPin6);}
+        else if(sensorIndex == 7) {objectInPlow = singleSensorDetect(trigPin7, echoPin7);}
+        else if(sensorIndex == 8) {objectInPlow = singleSensorDetect(trigPin8, echoPin8);}
         
 
         if (++sensorIndex > 8) { sensorIndex = 1;}//increment the index and set to 1 if over 8
-        prevMillis = millis(); //set previous milliseconds to test for next time funtion is run
+        prevMillisPulse = millis(); //set previous milliseconds to test for next time funtion is run
+        
     }
-
+    //debounce timer
+    if(objectInPlow){//return true of object detected
+        return true;
+        prevMillisDebounce = millis();
+    }
+    else if (currentMillis - sensorDebounce > prevMillisDebounce){return false;}//if object has not been detected in sensorDebounce time return false
+    
     return false;
 }
